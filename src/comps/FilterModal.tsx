8fce77cac4829baf01249e0afd4e3a119f5e0e84
Iranigami <iranigami@mail.ru>
2025-04-18 17:56:13 +0400

    commit

--- src/comps/FilterModal.tsx
+++ src/comps/FilterModal.tsx
@@ -52,8 +52,9 @@ export default function FilterModal({ onClose, opened }: Props) {
 
   return (
     <div
-      className={`ml-[-510px] ${opened && "translate-x-[510px]"} transition duration-300 w-[508px] h-[1080px] gap-[44px] bg-[url(src/assets/images/bg-modal.png)] fixed top-0 left-0 z-[1000] justify-center flex inline overflow-y-auto hide-scroll pl-[80px]`}
+      className={`ml-[-510px] ${opened && "translate-x-[510px]"} transition duration-300 w-[508px] h-[1080px] gap-[44px] bg-[url(src/assets/images/bg-modal.png)] fixed top-0 left-0 z-[1000] justify-center flex inline pl-[80px]`}
     >
+      <div className="overflow-y-auto hide-scroll h-[860px]">
       <div className="w-[388px] h-[64px] flex items-top justify-between gap-[121px] mt-[40px] border-b border-light-brown">
         <span className="font-normal text-[40px] leading-[100%] tracking-[4.8px] text-center uppercase text-brown">
           Фильтры
@@ -75,34 +76,64 @@ export default function FilterModal({ onClose, opened }: Props) {
           <input className="w-[184px] h-full border border-light-brown focus:outline-none p-[16px] font-normal text-[18px] leading-[100%] tracking-[2.16px] uppercase text-brown" />
           <input className="w-[184px] h-full border border-light-brown focus:outline-none p-[16px] font-normal text-[18px] leading-[100%] tracking-[2.16px] uppercase text-brown" />
         </div>
-        <div className="mt-[44px] w-[388px]">
-          <div className="font-normal text-[24px] leading-[100%] tracking-[2.9px] text-left uppercase text-brown mt-[44px] h-[28px]">
-            Звание
+      </div>
+      <div className="mt-[44px] w-[388px]">
+        <div className="font-normal text-[24px] leading-[100%] tracking-[2.9px] text-left uppercase text-brown mt-[44px] h-[28px]">
+          Звание
+        </div>
+        {tempFilter.rank.map((data: string, index: number) => (
+          <div
+            key={index}
+            onClick={() => {selectedRank===data ? setRank(undefined) : setRank(data)}}
+            className="font-normal flex text-[18px] leading-[100%] tracking-[2.16px] items-center gap-[16px] text-left uppercase text-brown mt-[16px] h-[28px]"
+          >
+            {selectedRank===data ? (
+              <div 
+                className="size-[24px] bg-red flex justify-center items-center">
+                <img src={check} alt="selected" className="size-[16px]" />
+              </div>
+            ) : (
+              <div
+                className="size-[24px] border border-light-brown"
+              />
+            )}
+            {data}
           </div>
-          {tempFilter.rank.map((data: string, index: number) => (
-            <div
-              key={index}
-              onClick={() => {selectedRank===data ? setRank(undefined) : setRank(data)}}
-              className="font-normal flex text-[18px] leading-[100%] tracking-[2.16px] items-center gap-[16px] text-left uppercase text-brown mt-[44px] h-[28px]"
-            >
-              {selectedRank===data ? (
-                <div 
-                  className="size-[24px] bg-red flex justify-center items-center">
-                  <img src={check} alt="selected" className="size-[16px]" />
-                </div>
-              ) : (
-                <div
-                  className="size-[24px] border border-light-brown"
-                />
-              )}
-              {data}
-            </div>
-          ))}
+        ))}
+      </div>
+      <div className="mt-[44px] w-[388px]">
+        <div className="font-normal text-[24px] leading-[100%] tracking-[2.9px] text-left uppercase text-brown mt-[44px] h-[28px]">
+          По буквам
+        </div>
+        <div className="grid grid-cols-7 gap-[16px] mt-[20px]">
+        {tempFilter.word.map((data: string, index: number) => (
+          <div
+            key={index}
+            onClick={() => {selectedWord===data ? setWord(undefined) : setWord(data)}}
+            className={`${selectedWord===data ? "bg-red flex text-white" : "border border-light-brown text-light-brown"} font-normal justify-center items-center flex text-[18px] leading-[100%] tracking-[2.16px] items-center gap-[16px] text-left uppercase text-brown size-[40px]`}
+          >
+            {data}
+          </div>
+        ))}
         </div>
       </div>
-      <div className="flex inline w-[388px] h-[220px] absolute left-[80px] bottom-0 justify-center">
-          <button className="w-full h-[69px] bg-red ml-">       {/*крч надо верхнуюю часть обернуть в див, а эту отдельно, там скролл будет именно у верхней части*/}
 
+      </div>
+      <div className="flex inline w-[388px] h-[220px] absolute left-[80px] bottom-0 justify-center">
+          <button 
+            onClick={()=>{}}
+            className="w-full h-[69px] bg-red font-normal flex text-[18px] leading-[100%] tracking-[2.16px] items-center text-center uppercase text-white justify-center flex">     
+            Применить
+          </button>
+          <button 
+            onClick={()=>{
+              setRank(undefined);
+              setWord(undefined);
+              startYear.current=0;
+              endYear.current=1946;
+            }}
+            className="w-full mt-[20px] h-[69px] border border-light-brown font-normal flex text-[18px] leading-[100%] tracking-[2.16px] items-center text-center uppercase text-brown justify-center flex">  
+            Очистить
           </button>
       </div>
     </div>
